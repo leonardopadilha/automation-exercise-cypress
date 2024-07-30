2088e26f040712d68957783add0e89f3dbc581d6
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-30 06:02:00 -0300

    Ajuste arquivo yml

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.automationexercise.com',
     watchForFileChanges: false,
+    fixturesFolder: 'cypress/fixtures',
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
